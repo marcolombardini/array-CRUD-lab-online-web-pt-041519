1a38e4ca98729e0133fa964a41b500df87e10800
Marco Lombardini <marco.lombardini@gmail.com>
2019-04-21 17:27:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array ["cats", "dogs"]
-  add_element_to_start_of_array
+  add_element_to_start_of_array.push("mice")
 end
 
 def add_element_to_start_of_array(array, element)
