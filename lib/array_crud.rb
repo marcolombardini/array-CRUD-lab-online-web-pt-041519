19f205125286619b2b2b82f2f817ebc6a0f29278
Marco Lombardini <marco.lombardini@gmail.com>
2019-04-21 18:31:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,8 +23,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-     array = ["arrey"]
-  array.unshift ("wow")
+     array = ["arrey", "arrays!"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
